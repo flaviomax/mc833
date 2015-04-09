e6bb77ef6a2d101f0c0302c2dd90c6081f884b73
crisfbazz <crisfbazz@gmail.com>
2015-04-09 12:35:56 -0300

    Gerar grafico versao 1

--- geragraficos.R
+++ geragraficos.R
@@ -1,11 +1,20 @@
-t1 <- read.table("test1.txt", sep = " ", col.names = c("tempoTotal", "tempoTransmissao", "check"))
+t1 <- read.table("results/test1.txt", sep = " ", col.names = c("tempoTotal", "tempoTransmissao", "check"))
+t2 <- read.table("results/test2.txt", sep = " ", col.names = c("tempoTotal", "tempoTransmissao", "check"))
+t3 <- read.table("results/test3.txt", sep = " ", col.names = c("tempoTotal", "tempoTransmissao", "check"))
+t4 <- read.table("results/test4.txt", sep = " ", col.names = c("tempoTotal", "tempoTransmissao", "check"))
+t5 <- read.table("results/test5.txt", sep = " ", col.names = c("tempoTotal", "tempoTransmissao", "check"))
+t6 <- read.table("results/test6.txt", sep = " ", col.names = c("tempoTotal", "tempoTransmissao", "check"))
+t7 <- read.table("results/test7.txt", sep = " ", col.names = c("tempoTotal", "tempoTransmissao", "check"))
 
-boxplot(t1$tempoTotal,group = t1$tempoTotal)
+t_total <- data.frame(t1 = t1$tempoTotal, t2 = t2$tempoTotal, t3 = t3$tempoTotal, t4 = t4$tempoTotal, t5=t5$tempoTotal, t6=t6$tempoTotal, t7=t7$tempoTotal)
 
-m1 <- mean(t1$tempoTotal)
-sd1 <- sd(t1$tempoTotal) 
-g1 <- ggplot(data=t1, aes(x=1:30, y=tempoTotal)) + 
-    geom_errorbar(aes(ymin = m1 - sd1, ymax = m1+sd1)) +
+t_t <- melt(t_total)
+means <- rep(tapply(t_t$value, t_t$variable, mean), each = 30)
+sds <- rep(tapply(t_t$value, t_t$variable, sd)*2, each = 30)
+t_t <- cbind(t_t, means, sds)
+
+g1 <- ggplot(data=t_t, aes(x=variable, y=value)) + 
+    geom_errorbar(aes(ymin = means - sds, ymax = means+sds)) +
     geom_point()
     
     
